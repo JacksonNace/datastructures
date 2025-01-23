741d7d939a23d82399bad64dfa6a2b03f35b602c
JacksonNace <jacksonnace964@gmail.com>
2025-01-23 10:24:40 -0600

    YES!

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+struct st{
+  int id;
+  int gpa;
+  st *next;
+};
+
+int main () {
+  st *p = new st;
+  st *head = p;
+  for (int i; i < 3; i++){
+    p->id=rand()%1000;
+    p->gpa = 1+rand()%4;
+    p->next = nullptr;
+    cout << "current node:" << i << endl;
+    cout << "current id:" << p->id << endl;
+    cout << "current gpa:" << p->gpa << endl;
+
+    p = p->next;
+    p = new st;
+  }
+
+  p->id = 3;
+  p->gpa=2.7;
+  p->next=nullptr;
+
+  return 0;
+};
